b03d4d53b78fa319dce892d507e0cfed5ae2aec8
Danss <kualfian123@gmail.com>
2022-12-27 21:19:33 +0700

    Initial

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GoogleOAuthProvider clientId='453345107768-62oamer07figpo2hbe2un0sulnp5s7cb.apps.googleusercontent.com'>
         <App />
       </GoogleOAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
